fca235926a80d7ad3a60ff3645169843fc5dcc9c
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-08-26 00:54:16 +0530

    `feat: landing header`

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import {Header} from "@/components/landing/Header";
+import Footer from "@/components/layout/Footer";
+import { Metadata } from "next";
+
+export const metadata:Metadata ={
+    title:{
+        template:"Iris Ui = Open Source Components",
+        default:"Iris Ui"
+    }
+}
+
+
+export default function HomeLayout({children}:{children:React.ReactNode}){
+    return (
+<>
+<Header/>
+<main className="relative w-full pt-0 md:pt-0">
+    
+    
+    {children}
+     </main>
+     <Footer/>
+</>
+)
+}
