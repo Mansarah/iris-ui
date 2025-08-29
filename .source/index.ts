87ad84d72cc0a757820acc35943ddcdc2472f759
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-08-29 23:15:16 +0530

    `make the configuration of fumadoc and make preview of mdx`

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1756481235293"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1756489338688"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/whapp/Desktop/ai-project/iris-ui/content/docs/index.mdx"}, data: docs_0 }], [])
+export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/whapp/Desktop/ai-project/iris-ui/content/docs/index.mdx"}, data: docs_0 }]);
+export const meta = _runtime.meta<typeof _source.meta>([]);
--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,21 @@
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+title: {
+template:
+"%s | irisui - Free UI Components to build beautiful websites",
+default: "irisui - Free UI Components to build beautiful websites",
+},
+};
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout tree={source.pageTree} {...baseOptions()} sidebar={{
+defaultOpenLevel: 1,
+
+}}>
       {children}
     </DocsLayout>
   );
